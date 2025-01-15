5edde6cbc1ba8c251a7ada5c23a7629163794a59
bogyeom0922 <pbk0398@gmail.com>
2025-01-15 18:16:31 +0900

    Fix: Fix: 데이터베이스 비밀번호 변경 및 form action 주소 수정 #3
    - 데이터베이스 연결 비밀번호 변경하여 문제 해결
    - form action 주소 수정하여 문제 해결

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,14 +7,17 @@ exports.renderSignupPage = (req, res) => {
 exports.handleSignup = (req, res) => {
     const {id, password, name, introduction} = req.body;
     const profileImage = req.file ? '/uploads/' + req.file.filename : null;
+    console.log('id:', id, 'password:', password, 'name:', name, 'introduction:', introduction, 'profileImage:', profileImage);
+
     const query = 'INSERT INTO user (id, password, name, introduction, profile_image) VALUES (?, ?, ?, ?, ?)';
     const params = [id, password, name, introduction, profileImage];
 
     executeQuery(query, params)
         .then(() => {
-            res.redirect('/');
+            res.redirect('/user/login');
         })
         .catch(error => {
+            console.error(error);
             res.status(500).send('Error handling signup');
         });
 };
