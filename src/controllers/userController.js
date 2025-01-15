d3d3662b801a9f62ac30e975189e5742b0bfb870
bogyeom0922 <pbk0398@gmail.com>
2025-01-15 18:01:24 +0900

    Fix: 회원가입 DB 요소 수정 및 프로필 이미지 처리 #3
    - 회원가입 DB 요소를 아이디, 비밀번호, 이름, 소개, 이미지로 변경
    - multer를 사용하여 미들웨어에서 프로필 이미지를 받아 DB에 저장되도록 수정
    - 회원 등록 버튼 클릭시 404 에러 발생, 수정 작업 중

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,14 @@
-const { executeQuery } = require('../services/dbService');
+const {executeQuery} = require('../services/dbService');
 
 exports.renderSignupPage = (req, res) => {
-    res.render('signup');
+    res.render('user/signup');
 };
 
 exports.handleSignup = (req, res) => {
-    const { id, password, name, introduction, question, answer } = req.body;
-    const query = 'INSERT INTO user (id, password, name, introduction, question, answer) VALUES (?, ?, ?, ?, ?, ?)';
-    const params = [id, password, name, introduction, question, answer];
+    const {id, password, name, introduction} = req.body;
+    const profileImage = req.file ? '/uploads/' + req.file.filename : null;
+    const query = 'INSERT INTO user (id, password, name, introduction, profile_image) VALUES (?, ?, ?, ?, ?)';
+    const params = [id, password, name, introduction, profileImage];
 
     executeQuery(query, params)
         .then(() => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // 애플리케이션 초기 설정
 const express = require('express');
+const path = require('path');  // path 모듈 추가
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
@@ -13,10 +14,14 @@ const app = express();
 
 // 기본 미들웨어
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '..', 'public')));  // public 폴더 경로 수정
+
 app.use(cookieParser());
 app.set('view engine', 'ejs');
+
+// 'views' 폴더 경로 수정
+app.set('views', path.join(__dirname, '..', 'views'));  // views 폴더 경로 수정
 
 // 라우터 설정
 app.use('/', homeRouter);
