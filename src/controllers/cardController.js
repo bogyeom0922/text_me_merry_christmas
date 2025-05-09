046ff13702120b86e7292e8083f0f3ba1dc9245d
bogyeom0922 <pbk0398@gmail.com>
2025-05-09 21:08:33 +0900

    Feat: 카드 전송/조회 흐름 개선 및 메뉴 구조 통합 #11
    - 카드 전송 관련 페이지를 하나로 통합하여 menu 기반으로 전환
    - 기존 sendcard.ejs를 카드 작성/조회 선택 메뉴 페이지로 변경
    - 카드 작성 시 그림 선택 흐름 추가 예정
    - 받은 카드와 보낸 카드를 구분하여 별도 목록으로 조회 가능하도록 구현
    - 전체 흐름을 '/card' 하위 경로로 정리하여 URL 구조 일관성 확보

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -1,68 +1,47 @@
 const Card = require('../models/card');
 
-// 카드 작성 페이지 렌더링
-exports.renderCardPage = (req, res) => {
-    const { type } = req.params;
-    res.render(`card/card${type}`);
+exports.renderCardMenu = (req, res) => {
+    res.render('card/menu');
 };
 
-// 카드 생성 처리
+exports.renderCreatePage = (req, res) => {
+    res.render('card/create');
+};
+
 exports.handleCreateCard = async (req, res) => {
-    const { from_name, to_name, content } = req.body;
+    const user = req.user || req.session.user;
+    const { to_name, content } = req.body;
     const cardType = req.params.type;
 
     try {
         const newCard = new Card({
-            from_name,
+            from_name: user.nickname,
             to_name,
             content,
-            card_type: cardType
+            card_type: "1"
         });
 
         await newCard.save();
-        res.redirect('/sendcard');
+        res.redirect('/card');
     } catch (error) {
         console.error('카드 생성 오류:', error);
         res.status(500).send('Error creating card');
     }
 };
 
-// 카드 리스트 보기
-exports.listCards = async (req, res) => {
-    try {
-        const cards = await Card.find();
-        res.render('list', { data: cards });
-    } catch (error) {
-        console.error('카드 리스트 오류:', error);
-        res.status(500).send('Error fetching card list');
-    }
+exports.renderSentCardList = async (req, res) => {
+    const user = req.user;
+    const cards = await Card.find({ from_name: user.nickname });
+    res.render('card/sentList', { data: cards });
 };
 
-// 카드 상세 보기
-exports.viewCard = async (req, res) => {
-    const { from_name } = req.params;
-
-    try {
-        const card = await Card.findOne({ from_name });
-
-        if (!card) return res.status(404).send('Card not found');
-        res.render('view', { data: card });
-    } catch (error) {
-        console.error('카드 조회 오류:', error);
-        res.status(500).send('Error fetching card');
-    }
+exports.renderReceivedCardList = async (req, res) => {
+    const user = req.user;
+    const cards = await Card.find({ to_name: user.nickname });
+    res.render('card/receivedList', { data: cards });
 };
 
-// 카드 내용 수정
-exports.updateCard = async (req, res) => {
-    const { from_name } = req.params;
-    const { content } = req.body;
-
-    try {
-        await Card.findOneAndUpdate({ from_name }, { content });
-        res.redirect('/');
-    } catch (error) {
-        console.error('카드 수정 오류:', error);
-        res.status(500).send('Error updating card');
-    }
+exports.renderCardDetail = async (req, res) => {
+    const card = await Card.findById(req.params.id);
+    res.render('card/detail', { data: card });
 };
